2c491f0800c291acbe459239ff80a53fc1170698
AminooData <Aminb732@gmail.com>
2025-02-06 14:20:12 +0100

    Je a l'exercice 4

---
+++ Crashcourse/Execice_Day_1.py
@@ -0,0 +1,40 @@
+#Exercise 1
+# Utilisation de '\n' pour ajouter un saut de ligne entre chaque "Hello world"
+print("Hello world\nHello world\nHello world\nHello world")
+
+#Exercise 2
+# Calcul de la puissance : 99 à la puissance 3
+puissance = 99 ** 3  
+
+# Multiplication du résultat par 8
+resultat = puissance * 8  
+
+# Affichage du résultat final
+print(resultat)
+
+#Exercise 3
+#  Demande à l'utilisateur d'entrer son prénom
+user_name = input("Quel est ton prénom ? ")  
+
+#  Notre prénom pour comparer
+my_name = "Amin"  
+
+#  Vérifie si l'utilisateur a le même prénom
+if user_name == my_name:  
+    print("Wow ! Nous avons le même prénom !")  #  Affiche ce message si les prénoms sont identiques
+else:  
+    print(f"Oh ! Enchanté, {user_name}, joli prénom !")  #  Affiche un message personnalisé
+
+#Exercise 4
+
+use_tall =int(input("Quelle taille faite vous ?"))
+
+tall = 145
+
+if use_tall >=  tall:
+    print("Vous pouvez monter à bord !")
+else:
+    print("Désolé, vous ne pouvez pas monter à bord !")
+    
+#Exercise 5
+    
